00a03fc6083d32ed85eb0be2d02ff40123b331fb
Aditya505520 <aditya505520@gmail.com>
2026-08-14 12:52:43 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "pdf-share-hub-v1";
+
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./admin.html",
+  "./assets/style.css",
+  "./assets/app.js",
+  "./assets/admin.js",
+  "./assets/config.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
